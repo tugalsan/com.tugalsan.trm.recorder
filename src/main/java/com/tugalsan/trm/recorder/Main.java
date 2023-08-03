f5f80495716df94f6516c4fdb713735217d9d821
tugalsan karabacak <tugalsan@gmail.com>
2023-08-03 15:06:13 +0300

    Update Main.java

--- src/main/java/com/tugalsan/trm/recorder/Main.java
+++ src/main/java/com/tugalsan/trm/recorder/Main.java
@@ -3,20 +3,22 @@ package com.tugalsan.trm.recorder;
 import com.tugalsan.api.input.server.*;
 import com.tugalsan.api.log.server.*;
 import com.tugalsan.api.thread.server.TS_ThreadWait;
+import com.tugalsan.api.thread.server.safe.TS_ThreadSafeTrigger;
 import java.nio.file.*;
 
 //WHEN RUNNING IN NETBEANS, ALL DEPENDENCIES SHOULD HAVE TARGET FOLDER!
 public class Main {
 
     final private static TS_Log d = TS_Log.of(Main.class);
+    final private static TS_ThreadSafeTrigger killTrigger = TS_ThreadSafeTrigger.of();
 
     //cd C:\me\codes\com.tugalsan\trm\com.tugalsan.trm.recorder
     //java --enable-preview --add-modules jdk.incubator.concurrent -jar target/com.tugalsan.trm.recorder-1.0-SNAPSHOT-jar-with-dependencies.jar
     public static void main(String[] args) {
         System.out.println("0");
-        var soundRecorder = TS_InputSound.of(Path.of("D:\\as\\a.wav"));
+        var soundRecorder = TS_InputSound.of(killTrigger, Path.of("D:\\as\\a.wav"));
         System.out.println("1");
-        TS_ThreadWait.seconds(null, 20f);
+        TS_ThreadWait.seconds(d.className, killTrigger, 20f);
         System.out.println("3");
         soundRecorder.kill();
         System.out.println("5");
