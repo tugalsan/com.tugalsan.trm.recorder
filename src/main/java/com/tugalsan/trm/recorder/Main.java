1a813f9d094b8822db833baf2fffbb071e530750
tugalsan karabacak <tugalsan@gmail.com>
2023-08-03 20:36:23 +0300

    .

--- src/main/java/com/tugalsan/trm/recorder/Main.java
+++ src/main/java/com/tugalsan/trm/recorder/Main.java
@@ -3,14 +3,14 @@ package com.tugalsan.trm.recorder;
 import com.tugalsan.api.input.server.*;
 import com.tugalsan.api.log.server.*;
 import com.tugalsan.api.thread.server.TS_ThreadWait;
-import com.tugalsan.api.thread.server.safe.TS_ThreadSafeTrigger;
+import com.tugalsan.api.thread.server.sync.TS_ThreadSyncTrigger;
 import java.nio.file.*;
 
 //WHEN RUNNING IN NETBEANS, ALL DEPENDENCIES SHOULD HAVE TARGET FOLDER!
 public class Main {
 
     final private static TS_Log d = TS_Log.of(Main.class);
-    final private static TS_ThreadSafeTrigger killTrigger = TS_ThreadSafeTrigger.of();
+    final private static TS_ThreadSyncTrigger killTrigger = TS_ThreadSyncTrigger.of();
 
     //cd C:\me\codes\com.tugalsan\trm\com.tugalsan.trm.recorder
     //java --enable-preview --add-modules jdk.incubator.concurrent -jar target/com.tugalsan.trm.recorder-1.0-SNAPSHOT-jar-with-dependencies.jar
